faed3449fc21381ce9583a28e8470f388d612e14
Weidong Xu <weidxu@microsoft.com>
2020-03-05 17:28:39 +0800

    parallel=1

--- regen_code.sh
+++ regen_code.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-gulp codegen --autorest 3.0.6237 --projects resources,storage,network,compute,appservice,sql --spec-root ../azure-rest-api-specs --autorest-java ../autorest.java/fluentgen
+gulp codegen --parallel 1 --autorest 3.0.6237 --projects resources,storage,network,compute,appservice,sql --spec-root ../azure-rest-api-specs --autorest-java ../autorest.java/fluentgen
 
